713b4f48a65b23e70cef070d910d65058bd18a4e
galevi <gall.levi.tech@gmail.com>
2020-03-05 15:07:04 -0600

    Module 3

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
+def open_connection():
+    connection = g.__getattr__('_connection')
 
 @app.route('/')
 @app.route('/jobs')
